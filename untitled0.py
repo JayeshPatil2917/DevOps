d8722e0d87c8d2110ecf035f3973cffbffae5746
JayeshPatil2917 <oops.hiii2417@gmail.com>
2024-08-12 14:19:05 +0530

    Exam Pratical

---
+++ untitled0.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Feb 13 12:59:24 2024
+
+@author: Jay
+"""
+
+num = 2
+while True:
+    num1 = int(input("Enter a number : "))
+    if num1 == 2:
+        break
+    num+=num1
+    
